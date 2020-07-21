ba549aadfd16113340ccdf08ecb9e403c5646280
pharia-le <pharia.le@gmail.com>
2020-07-20 20:05:23 -0700

    Update session store initializer w/ dynamic domain property.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_productivity_app", domain: "http://localhost:3001"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_productivity_app", domain: "http://localhost:3001"
+else
+    Rails.application.config.session_store :cookie_store, key: "_productivity_app"
+end
