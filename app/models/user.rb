9ed314db77dc44dfff9b914d826e77ae79b0ad1a
pharia-le <pharia.le@gmail.com>
2020-07-22 13:08:51 -0700

    Update models to include Log relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    belongs_to :user
+    has_many :habits
+    has_many :logs, through: :habits
 
     validates_presence_of :email
     validates_uniqueness_of :email
