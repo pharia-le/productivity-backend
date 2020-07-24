247acf22ff79db31609eb37bc9911a7158f7b1de
pharia-le <pharia.le@gmail.com>
2020-07-23 18:19:41 -0700

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+emails = [
+    'user_1@example.com',
+    'user_2@example.com',
+    'user_3@example.com',
+    'user_4@example.com',
+    'user_5@example.com'
+]
+
+emails.each { |email|
+    User.create(
+    email: email,
+    password: 'password'
+    )
+}
+
+habits = [
+    'Meditate',
+    'Exercise',
+    'Drink 6 Cups of Water',
+    'Stay Home'
+]
+
+dates = [
+    Date.today,
+    Date.yesterday
+]
+
+User.all.each { |user|
+    habits.each { |habit|
+        user.habits.build(
+            name: habit
+        )
+    }
+    user.save
+}
+
+Habit.all.each { |habit| 
+    dates.each { |date|
+        habit.logs.build(
+            date_time: date
+        )
+    }
+    habit.save
+}
