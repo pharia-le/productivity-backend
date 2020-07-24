aac355a89b1e53d8feb93ba010e3fcd82de6c507
pharia-le <pharia.le@gmail.com>
2020-07-23 18:24:30 -0700

    Update controllers & redered json.

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,73 +1,48 @@
 class HabitsController < ApplicationController
   before_action :set_habit, only: [:show, :edit, :update, :destroy]
 
-  # GET /habits
-  # GET /habits.json
   def index
     @habits = Habit.all
+    render json: @habits
   end
 
-  # GET /habits/1
-  # GET /habits/1.json
   def show
+    render json: @habit
   end
 
-  # GET /habits/new
   def new
     @habit = Habit.new
   end
 
-  # GET /habits/1/edit
   def edit
   end
 
-  # POST /habits
-  # POST /habits.json
   def create
     @habit = Habit.new(habit_params)
-
-    respond_to do |format|
-      if @habit.save
-        format.html { redirect_to @habit, notice: 'Habit was successfully created.' }
-        format.json { render :show, status: :created, location: @habit }
-      else
-        format.html { render :new }
-        format.json { render json: @habit.errors, status: :unprocessable_entity }
-      end
+    if @habit.save
+      render :show, status: :created, location: @habit
+    else
+      render json: @habit.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /habits/1
-  # PATCH/PUT /habits/1.json
   def update
-    respond_to do |format|
-      if @habit.update(habit_params)
-        format.html { redirect_to @habit, notice: 'Habit was successfully updated.' }
-        format.json { render :show, status: :ok, location: @habit }
-      else
-        format.html { render :edit }
-        format.json { render json: @habit.errors, status: :unprocessable_entity }
-      end
+    if @habit.update(habit_params)
+      render :show, status: :ok, location: @habit
+    else
+      render json: @habit.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /habits/1
-  # DELETE /habits/1.json
   def destroy
     @habit.destroy
-    respond_to do |format|
-      format.html { redirect_to habits_url, notice: 'Habit was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_habit
       @habit = Habit.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def habit_params
       params.require(:habit).permit(:name, :user_id)
     end
